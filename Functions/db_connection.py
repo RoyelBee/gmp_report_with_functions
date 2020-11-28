a94d6422e1effb27d4b878c15d36dd6772dd81be
Rejaul Islam Royel <royelsoft@gmail.com>
2020-11-28 10:08:37 +0600

    Delete db_connection.py

--- Functions/db_connection.py
+++
@@ -1,6 +0,0 @@
-import pyodbc as db
-
-connection = db.connect('DRIVER={SQL Server};'
-                            'SERVER=137.116.139.217;'
-                            'DATABASE=ARCHIVESKF;'
-                            'UID=sa;PWD=erp@123')
